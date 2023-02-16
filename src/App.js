e46e09dc59ffbbb2c9db962154fedfa2ed162395
mosab01 <mosabkhraim3@gmail.com>
2023-02-17 00:50:32 +0200

    main

--- src/App.js
+++ src/App.js
@@ -4,39 +4,56 @@ import { useState } from 'react';
 function App() {
   const [data, setData] = useState("");
   const [color, setColor] = useState("");
+  const [bgcolor, setbgColor] = useState("");
+
 
   const set_data = () =>{
-    console.log(data);
-    console.log(color);
-    return(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${data}&color=${color}`);
+    return(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${data}&color=${color}&bgcolor=${bgcolor}&format=png`);
   }
 
 
  
   return (
     <div className="App">
-      <h1>QR Generator</h1>
-      <img src={set_data()}/>
+      <h1 id='title'>QR Generator</h1>
+      <img src={set_data()} alt="Type somthing"/>
       <div className='input-fields'>
 
-        <div className='data-entry'>
+        <div className='input-field'>
+          <label>URL Name: </label>
           <input 
-          id='data'
-          placeholder='url name' 
+          id='data' 
           value={data} 
-          onChange={(e) => setData(e.target.value)}/>
-          <button onClick={() => set_data(data)}>click me</button>
+          onChange={(e) => setData(e.target.value)}
+          />
         </div>
 
-        <div className='color-entry'>
+        <div className='input-field'>
+          <label>Color: </label>
           <input 
           id='color'
-          placeholder='color' 
           value={color}
-          onChange={(e) => setColor(e.target.value)}/>
-          <button onClick={() => set_data(color)}>click me</button>
+          onChange={(event) => setColor(event.target.value)}
+          />
         </div>
+
+        <div className='input-field'>
+          <label>BgColor: </label>
+          <input 
+          id='bgcolor'
+          value={bgcolor}
+          onChange={(event) => setbgColor(event.target.value)}
+          />
+        </div>
+
       </div>
+
+      <a id='download-btn' href={set_data()} download="download" target="_blank">
+        <button>download
+        </button>
+        </a>
+
+
     </div>
   );
 }
