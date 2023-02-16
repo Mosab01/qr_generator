4f133fdbddd423eafe6cd316e832618c9e532d18
mosa01 <mosabkhraim3@gmail.com>
2023-02-16 18:28:10 +0200

    main

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [data, setData] = useState("");
+  const [color, setColor] = useState("");
 
-  function Set_values(segment){
-    let API_KEY = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${segment}`;
-    console.log(segment);
-    return(
-      API_KEY
-    );
+  const set_data = () =>{
+    console.log(data);
+    console.log(color);
+    return(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${data}&color=${color}`);
   }
 
 
+ 
   return (
     <div className="App">
       <h1>QR Generator</h1>
-      <img src={Set_values()}/>
+      <img src={set_data()}/>
+      <div className='input-fields'>
+
+        <div className='data-entry'>
+          <input 
+          id='data'
+          placeholder='url name' 
+          value={data} 
+          onChange={(e) => setData(e.target.value)}/>
+          <button onClick={() => set_data(data)}>click me</button>
+        </div>
+
+        <div className='color-entry'>
+          <input 
+          id='color'
+          placeholder='color' 
+          value={color}
+          onChange={(e) => setColor(e.target.value)}/>
+          <button onClick={() => set_data(color)}>click me</button>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
