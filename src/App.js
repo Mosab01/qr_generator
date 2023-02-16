a5c2d065f6c4cb3f33d596e58f11d5384e3cb41d
mosa01 <mosabkhraim3@gmail.com>
2023-02-16 15:57:54 +0200

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function Set_values(segment){
+    let API_KEY = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${segment}`;
+    console.log(segment);
+    return(
+      API_KEY
+    );
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>QR Generator</h1>
+      <img src={Set_values()}/>
     </div>
   );
 }
